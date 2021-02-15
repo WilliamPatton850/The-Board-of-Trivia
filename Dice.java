d306b1a50a11d310521888729cf76f88d9cc52bb
Tri Pham <30403578+Pham2000@users.noreply.github.com>
2021-02-15 13:47:56 -0600

    Dice class

---
+++ Dice.java
@@ -0,0 +1,12 @@
+public class Dice {
+	public static int roll() {
+		int dice1;
+		int dice2;
+		
+		dice1 = (int)(Math.random()*6+1);
+		dice2 = (int)(Math.random()*6+1);
+		
+		return dice1+dice2;
+		
+	}
+}
