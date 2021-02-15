fa230078464276f8e47d9bb9faa15a72e1b74f5e
Tri Pham <30403578+Pham2000@users.noreply.github.com>
2021-02-15 13:56:58 -0600

    Player class

---
+++ Player.java
@@ -0,0 +1,15 @@
+public class Player {
+	private String username;
+	private String password;
+	
+	public Player(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String playerName() {
+		return username;
+		
+	}
+	
+}
