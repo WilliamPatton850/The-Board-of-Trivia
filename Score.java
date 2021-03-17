21f4a76f8d331b2915ebdf4a1e1b7da694f58140
Cody <Cody@williams-mac-pro.local>
2021-03-17 15:21:32 -0500

    updated score class 2

--- Score.java
+++ Score.java
@@ -1,6 +1,50 @@
-//Testing 
-public class Score {
+//calculates and stores game scores
+//calculates and stores player rankings 
+//methods for easy, medium and hard questions (each worth different points) 
 
-	//Testing
+public class Score {
+	
+  private int playerID;
+  private int playerScore;
+  private int playerRanking;
+  
+//constructor 
+public Score(int playerID) {
+  this.playerID = playerID;
+  this.playerScore = 0;
+  this.playerRanking = 0;
+}
+	
+//easy questions (each question worth 5 points) 
+public void easyQuestion() {
+  playerScore = playerScore + 5;
+}
+  
+//medium questions (each question worth 10 points)
+public void mediumQuestion() {
+	playerScore = playerScore + 10;
+}
+	
+//hard questions (each question worth 15 points)
+public void hardQuestion() {
+	playerScore = playerScore + 15;
+}
+
+//returns player score 
+public int getScore() {
+  return playerScore; 
+}
+
+//returns player ranking 
+public int getRanking() {
+ return playerRanking;	
+}
+
+//calculate player ranking
+public int calculateRanking() {
+  //calculate ranking
+  return playerRanking; 
+}
+
 	
 }
