422d4da0b1345bb3470a6af32fe61f9f26a29918
JiyuHuang2018 <59706612+JiyuHuang2018@users.noreply.github.com>
2021-04-26 04:59:15 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+package triv;
+
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
@@ -9,7 +11,10 @@ public class Game {
 	static HashMap <Integer,ArrayList<JPanel>> tiles = new HashMap<>();
 	static int numPlayer;
 	static int currPlayer;
+	static HashMap<Integer,Integer> playerScore = new HashMap<>();
 	static HashMap<Integer,Integer> playerPlace = new HashMap<>();
+	JLabel rolled;
+	QuestionList qList = new QuestionList();
 	
 	Game(int numP){
 		numPlayer =numP ;
@@ -20,7 +25,7 @@ public class Game {
 	}
 	void startGame() {
 		JFrame game = new JFrame();
-		Dimension boardSize = new Dimension(400, 400);
+		Dimension boardSize = new Dimension(600, 600);
 		chessBoard = new JPanel();
 		chessBoard.setLayout( new GridLayout(8, 8,20,20) );
 		chessBoard.setPreferredSize( boardSize );
@@ -28,11 +33,10 @@ public class Game {
 		getSquares();
 		game.add(chessBoard);
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		game.setSize(900,700);
+		game.setSize(1400,1000);
 		game.setVisible(true);
 	}
 	void getSquares() {
-
 		for (int i = 0; i < 64; i++) {
 			JPanel square = new JPanel( new BorderLayout() );
 			square.setLayout(new GridLayout(2,8));
@@ -41,8 +45,40 @@ public class Game {
 			square.setBounds(0,0,pSize.width,pSize.height);
 			chessBoard.add( square );
 			int row = (i / 8) % 2;
+			if(i==17) {
+				JLabel p1 = new JLabel("Player 1 score: 0");
+				square.add(p1);
+				playerScore.put(1, 0);
+			}
+			if(i==18) {
+				JLabel p2 = new JLabel("Player 2 score: 0");
+				square.add(p2);
+				playerScore.put(2, 0);
+			}
+			if(i==19) {
+				JLabel p3 = new JLabel("Player 3 score: 0");
+				square.add(p3);
+				playerScore.put(3, 0);
+			}
+			if(i==20 && numPlayer>3) {
+				JLabel p4 = new JLabel("Player 4 score: 0");
+				square.add(p4);
+				playerScore.put(4, 0);
+			}
+			if(i == 21 && numPlayer >4) {
+				JLabel p5 = new JLabel("Player 5 score: 0");
+				square.add(p5);
+				playerScore.put(5, 0);
+			}
+			if(i==22 && numPlayer >5) {
+				JLabel p6 = new JLabel("Player 6 score: 0");
+				square.add(p6);
+				playerScore.put(6, 0);
+			}
 			if(i==27) {
 				square.setLayout(new GridLayout(1,1));
+				rolled = new JLabel("Hit dice to start!");
+				square.add(rolled);
 			}
 			if(i==28) {
 				square.setLayout(new GridLayout(1,1));
@@ -50,43 +86,7 @@ public class Game {
 				dice.addActionListener(new ActionListener(){
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						Dice d = new Dice();
-						int roll = d.roll();
-						tiles.get(playerPlace.get(currPlayer)).get(currPlayer).setBackground(Color.blue);
-						Color c;
-						if(currPlayer==0) {
-							c = Color.black;
-						}
-						else if(currPlayer == 1) {
-							c = Color.green;
-						}
-						else if(currPlayer ==2) {
-							c = Color.orange;
-						}
-						else if(currPlayer == 3) {
-							c = Color.gray;
-						}
-						else if(currPlayer == 4) {
-							c = Color.cyan;
-						}
-						else{
-							c = Color.magenta;
-						}
-						int place;
-						if(playerPlace.get(currPlayer)+roll>27) {
-							place = playerPlace.get(currPlayer)+roll-27;
-						}
-						else {
-							place = playerPlace.get(currPlayer)+roll;
-						}
-						tiles.get(place).get(currPlayer).setBackground(c);
-						playerPlace.put(currPlayer, place);
-						if(currPlayer==numPlayer-1) {
-							currPlayer =1;
-						}
-						else {
-							currPlayer++;
-						}
+						chooseHardness();
 					}
 				});
 				square.add(dice);
@@ -134,6 +134,265 @@ public class Game {
 			}
 			}
 		}
+	}
+	void chooseHardness() {
+		JPanel levelPanel = new JPanel();
+		JFrame levelFrame = new JFrame("choose your question type");
+		
+		JButton easy = new JButton("easy");
+		JButton medium = new JButton("medium");
+		JButton hard = new JButton("hard");
+		
+		easy.setFont(new Font("TimesRoman",Font.BOLD,30));
+		easy.setBounds(325,200,160,70);
+		
+		medium.setFont(new Font("TimesRoman",Font.BOLD,30));
+		medium.setBounds(325,300,160,70);
+		
+		hard.setFont(new Font("TimesRoman",Font.BOLD,30));
+		hard.setBounds(325,400,160,70);
+		
+		easy.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				levelFrame.setVisible(false);
+				getQuestion('e');
+			}
+		});
+		medium.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				levelFrame.setVisible(false);
+				getQuestion('m');
+			}
+		});
+		hard.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				levelFrame.setVisible(false);	
+				getQuestion('h');
+			}
+		});
+		levelPanel.add(easy);
+		levelPanel.add(medium);
+		levelPanel.add(hard);
+		levelPanel.setLayout(null);
+		
+		levelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		levelFrame.add(levelPanel);
+		levelFrame.setSize(800,600);
+		levelFrame.setVisible(true);
+	}
+	void getQuestion(Character hardness) {
+		qList.importAll();
+		Question q;
+		int correctAns;
+		JPanel qPanel = new JPanel();
+		JFrame qFrame = new JFrame("question");
+		if(hardness.equals('e')) {
+			q = qList.getEasyQ();
+		}
+		else if (hardness.equals('m')) {
+			q = qList.getMedQ();
+		}
+		else {
+			q = qList.getHardQ();
+		}
+		correctAns = q.getCorrectAnswer();
+		
+		Dice d = new Dice();
+		int roll = d.roll();
+		
+		JLabel rolled = new JLabel("Player"+currPlayer+" rolled " + roll);
+		rolled.setLocation(4,4);
+		rolled.setFont(new Font("TimesRoman",Font.BOLD,30));
+		Dimension h = rolled.getPreferredSize();
+		rolled.setBounds(285,100,h.width,h.height);
+		
+		JLabel question = new JLabel(q.getQuestion());
+		question.setLocation(4,4);
+		question.setFont(new Font("TimesRoman",Font.BOLD,30));
+		Dimension size = question.getPreferredSize();
+		question.setBounds(285,200,size.width,size.height);
+		
+		JButton ans1 = new JButton(q.getAnswerOne());
+		ans1.setFont(new Font("TimesRoman",Font.BOLD,30));
+		ans1.setBounds(325,300,700,100);
+		
+		JButton ans2 = new JButton(q.getAnswerTwo());
+		ans2.setFont(new Font("TimesRoman",Font.BOLD,30));
+		ans2.setBounds(325,400,700,100);
+		
+		JButton ans3 = new JButton(q.getAnswerThree());
+		ans3.setFont(new Font("TimesRoman",Font.BOLD,30));
+		ans3.setBounds(325,500,700,100);
+		
+		JButton ans4 = new JButton(q.getAnswerFour());
+		ans4.setFont(new Font("TimesRoman",Font.BOLD,30));
+		ans4.setBounds(325,600,700,100);
+		
+		ans1.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				qFrame.setVisible(false);
+				if(correctAns==1) {
+					updatePlayer(hardness,roll);
+				}
+				else {
+					showWrong(hardness,roll);
+				}
+			}
+		});
+		ans2.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				qFrame.setVisible(false);
+				if(correctAns==2) {
+					updatePlayer(hardness,roll);
+				}
+				else {
+					showWrong(hardness,roll);
+				}
+			}
+		});
+		ans3.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				qFrame.setVisible(false);
+				if(correctAns==3) {
+					updatePlayer(hardness,roll);
+				}
+				else {
+					showWrong(hardness,roll);
+				}
+			}
+		});
+		ans4.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				qFrame.setVisible(false);
+				if(correctAns==4) {
+					updatePlayer(hardness,roll);
+				}
+				else {
+					showWrong(hardness,roll);
+				}
+			}
+		});
+		
+		qPanel.add(rolled);
+		qPanel.add(question);
+		qPanel.add(ans1);
+		qPanel.add(ans2);
+		qPanel.add(ans3);
+		qPanel.add(ans4);
+		qPanel.setLayout(null);
+		
+		qFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		qFrame.add(qPanel);
+		qFrame.setSize(1200,1000);
+		qFrame.setVisible(true);
+	}
+	void showWrong(char hardness, int roll) {
+		if(hardness=='e') {
+			
+		}
+		else if(hardness =='m') {
+			tiles.get(playerPlace.get(currPlayer)).get(currPlayer).setBackground(Color.blue);
+			Color c;
+			if(currPlayer==0) {
+				c = Color.black;
+			}
+			else if(currPlayer == 1) {
+				c = Color.green;
+			}
+			else if(currPlayer ==2) {
+				c = Color.orange;
+			}
+			else if(currPlayer == 3) {
+				c = Color.gray;
+			}
+			else if(currPlayer == 4) {
+				c = Color.cyan;
+			}
+			else{
+				c = Color.magenta;
+			}
+			int place;
+			if(playerPlace.get(currPlayer)+roll>27) {
+				place = playerPlace.get(currPlayer)+roll-27;
+			}
+			else {
+				place = playerPlace.get(currPlayer)+roll;
+			}
+			tiles.get(place).get(currPlayer).setBackground(c);
+			playerPlace.put(currPlayer, place);
+			rolled.setText("Player"+ (currPlayer+1)+" Rolled "+ roll);
+			int score;
+			score =playerScore.get(currPlayer) ;
+			playerScore.put(currPlayer, score-roll);
+		}
+		else {
+			int score;
+			score =playerScore.get(currPlayer) ;
+			playerScore.put(currPlayer, score-roll);
+		}
+		if(currPlayer==numPlayer-1) {
+			currPlayer =0;
+		}
+		else {
+			currPlayer++;
+		}
+	}
+	void updatePlayer(char hardness,int roll) {
+		tiles.get(playerPlace.get(currPlayer)).get(currPlayer).setBackground(Color.blue);
+		Color c;
+		if(currPlayer==0) {
+			c = Color.black;
+		}
+		else if(currPlayer == 1) {
+			c = Color.green;
+		}
+		else if(currPlayer ==2) {
+			c = Color.orange;
+		}
+		else if(currPlayer == 3) {
+			c = Color.gray;
+		}
+		else if(currPlayer == 4) {
+			c = Color.cyan;
+		}
+		else{
+			c = Color.magenta;
+		}
+		int place;
+		if(playerPlace.get(currPlayer)+roll>27) {
+			place = playerPlace.get(currPlayer)+roll-27;
+		}
+		else {
+			place = playerPlace.get(currPlayer)+roll;
+		}
+		tiles.get(place).get(currPlayer).setBackground(c);
+		playerPlace.put(currPlayer, place);
+		rolled.setText("Player"+ (currPlayer+1)+" Rolled "+ roll);
+		if(currPlayer==numPlayer-1) {
+			currPlayer =0;
+		}
+		else {
+			currPlayer++;
+		}
+		int score;
+		score =playerScore.get(currPlayer) ;
+		if(hardness == 'e') {
+			playerScore.put(currPlayer, score+roll);
+		}
+		else if(hardness =='m') {
+			playerScore.put(currPlayer, (score+roll)*2);
+		}
+		else {
+			playerScore.put(currPlayer, (score+roll)*3);
+		}
+		
 	}
 	void tileAdd(int place, ArrayList<JPanel> t) {
 		switch(place) {
